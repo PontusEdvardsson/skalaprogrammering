c6c835cf3f95abc991ce654d412a35a244b67f05
pontus.edvardsson <pontuscvedvardsson@gmail.com>
2026-04-24 10:43:55 +0200

    Review solution library

--- ak9/sida_275/04_antal_tior.py
+++ ak9/sida_275/04_antal_tior.py
@@ -2,10 +2,9 @@ import random
 
 antal_tior = 0
 
-for _ in range(10000):
+for n in range(10000):
     slumptal = random.randint(1, 10)
     if slumptal == 10:
         antal_tior = antal_tior + 1
 
 print("Antalet tior:", antal_tior)
-
--- ak9/sida_279/26_sparande_med_ranta.py
+++ ak9/sida_279/26_sparande_med_ranta.py
@@ -1,7 +1,6 @@
 saldo = 0
 
-for _ in range(10):
+for n in range(10):
     saldo = saldo * 1.03 + 10000
 
 print("Det finns", round(saldo, 2), "kr pa kontot direkt efter den 10:e insattningen.")
-
